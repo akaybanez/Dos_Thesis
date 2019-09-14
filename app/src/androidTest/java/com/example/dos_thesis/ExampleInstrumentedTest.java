9fe463f5db0985787d51df1514fcd6fd113aa2d2
Jonna <jonna1114.mjb@gmail.com>
2019-09-15 00:32:39 +0800

    deleted support test libraries

--- app/src/androidTest/java/com/example/dos_thesis/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/dos_thesis/ExampleInstrumentedTest.java
@@ -1,8 +1,6 @@
 package com.example.dos_thesis;
 
 import android.content.Context;
-import android.support.test.InstrumentationRegistry;
-import android.support.test.runner.AndroidJUnit4;
 
 import androidx.test.InstrumentationRegistry;
 import androidx.test.runner.AndroidJUnit4;
